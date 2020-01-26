d6852a4e7d7079ea2b987f6d5224bf21415c5b7a
Amanda Barboza <amanda.brbz@hotmail.com>
2020-01-26 15:24:49 -0300

    webstorage

---
+++ js-ES6/webStorage/script.js
@@ -0,0 +1,33 @@
+//local storage
+/*salva internamente no navegador tipo cookie*/
+
+/*
+localStorage.setItem("nome", "Amanda"); //para guardar
+localStorage.getItem("nome"); //chamar pela chave
+localStorage.removeItem("nome"); //remover
+
+localStorage.nome = "Amanda";
+*/
+
+//session storage
+/* salva enquanto seu navegador está aberto, na sessão. */
+
+/*
+sessionStorage.setItem("nome", "Amanda"); //para guardar
+sessionStorage.getItem("nome"); //chamar pela chave
+sessionStorage.removeItem("nome"); //remover
+
+sessionStorage.nome = "Amanda";
+*/
+
+
+
+var nome = '';
+
+if(typeof localStorage.nome == 'undefined') {
+    localStorage.nome = prompt('digite seu nome');
+}
+
+nome = localStorage.nome;
+
+document.getElementById('nome').innerHTML = nome;
